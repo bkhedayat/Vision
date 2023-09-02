10a8e204d3c1738b9806edb162924bdd64cf9ee5
Amirshayan1 <sh.tatari18@gmail.com>
2023-09-02 22:21:15 +0200

    Fixed ConvLayer comments and added Bottleneck class

--- model/backbone.py
+++ model/backbone.py
@@ -4,10 +4,10 @@ import torch.nn.functional as F
 
 class ConvLayer(nn.Module):
     # class ConvLayer constructs a conv layer, BatchNorm and SiLU constructed with args:
-    # [layer_num, c_in, c_out, kernel, stride, padding, groups, dilation]
-    def __init__(self, num, c_in, c_out, k=1, s=1, p=None, g=1, d=1, activation=True):
+    # [layer_name, input_channel, output_channel, kernel, stride, padding, groups, dilation]
+    def __init__(self, layer_name, c_in, c_out, k=1, s=1, p=None, g=1, d=1, activation=True):
         super().__init__()
-        self.index = num
+        self.index = layer_name
         self.conv = nn.Conv2d(c_in, c_out, k, s, p, groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c_out)
         self.activation = nn.SiLU() if activation is True else nn.Identity()
@@ -18,4 +18,18 @@ class ConvLayer(nn.Module):
         x = self.bn(x)
         x = self.activation(x)
         return x
+    
+    def get_index(self):
+        print(self.index)
 
+class Bottleneck(nn.Module):
+    # class Bottleneck implements the block to reduce the number of channels in feature map using args:
+    # [input_channel, output_channel, groups, c_hidden_ratio]
+    def __init__(self, c_in, c_out, g=1, e=0.5):
+        super().__init__()
+        c_hidden = int(e * c_out)
+        self.conv1 = ConvLayer("bottleneck_1", c_in, c_hidden, 1, 1)
+        self.conv2 = ConvLayer("bottleneck_2", c_hidden, c_out, 3 ,1, g=g)
+
+    def forward(self, x):
+        return self.conv2(self.conv1(x))
