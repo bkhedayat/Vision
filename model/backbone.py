92a74cc9e1749b17707444949c0ee18018337442
Amirshayan1 <sh.tatari18@gmail.com>
2023-09-02 21:57:33 +0200

    CBS changed to ConvLayer

--- model/backbone.py
+++ model/backbone.py
@@ -2,13 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CBS_block(nn.Module):
-    # class CBS to provide Conv layer, BatchNorm and SiLU constructed with args:
+class ConvLayer(nn.Module):
+    # class ConvLayer constructs a conv layer, BatchNorm and SiLU constructed with args:
     # [layer_num, c_in, c_out, kernel, stride, padding, groups, dilation]
     def __init__(self, num, c_in, c_out, k=1, s=1, p=None, g=1, d=1, activation=True):
         super().__init__()
         self.index = num
-        self.conv = nn.Conv2d(c_in, c_out, k, s, p, d, g)
+        self.conv = nn.Conv2d(c_in, c_out, k, s, p, groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c_out)
         self.activation = nn.SiLU() if activation is True else nn.Identity()
     
@@ -18,3 +18,4 @@ class CBS_block(nn.Module):
         x = self.bn(x)
         x = self.activation(x)
         return x
+
