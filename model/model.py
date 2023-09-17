86f8ae6c77e3b07108409eabca201217afa67454
Amirshayan1 <sh.tatari18@gmail.com>
2023-09-17 20:34:04 +0200

    added Model class

--- model/model.py
+++ model/model.py
@@ -1,4 +1,7 @@
 import yaml
+import torch
+import torch.nn as nn
+from copy import deepcopy
 
 
 class YamlParser:
@@ -9,3 +12,13 @@ class YamlParser:
     def parse_config(self):
         with open(self.file_name) as f:
             model_config = yaml.load(f, Loader=yaml.FullLoader)
+
+
+class Model(nn.Module):
+    # class model get yaml config file and parse it. Creates backbone, neck and head components and fuses them
+    # ch_number is the number of channels of the input images
+    # class_number defines how many classes model can detect 
+    def __init__(self, config="config.yaml", ch_number=3, class_number=1):
+        super().__init__()
+        self.parser = YamlParser(deepcopy(config))
+        self.model = self.parser.parse_config()
