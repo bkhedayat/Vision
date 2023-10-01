8f2768ea9bc681458855204278231b82d465f4e0
Amirshayan1 <sh.tatari18@gmail.com>
2023-10-01 22:10:47 +0200

    added init function in train.py

--- train.py
+++ train.py
@@ -2,6 +2,13 @@ import sys
 import os
 from Base.yolo import Model
 
+def init():
+    sw_version = 0.1
+    print_version(sw_version)
+    
+    # add paths to systems
+    add_paths()
+
 def add_paths():
     # adds all the necessary paths to the system path
     model_dir = os.getcwd() + "\\Base"
@@ -18,12 +25,14 @@ def train():
     pass
 
 def print_version(version):
-    print(version)
+    print("\n ###############################################\n")
+    print("         TAKAM, Software version: {}".format(version))
+    print("\n ###############################################\n")
 
 if __name__ == "__main__":
 
-    # add the paths
-    add_paths()
+    # initialize the software
+    init()
 
     # create the yolo model
     model = create_yolo_model()
