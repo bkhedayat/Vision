f11e11841e1f9d297658246c3ea2d7e9933dd090
Amirshayan1 <sh.tatari18@gmail.com>
2023-10-02 23:27:54 +0200

    ConvLayer name removed

--- Base/backbone.py
+++ Base/backbone.py
@@ -5,9 +5,8 @@ import torch.nn.functional as F
 class ConvLayer(nn.Module):
     # class ConvLayer constructs a conv layer, BatchNorm and SiLU constructed with args:
     # [layer_name, input_channel, output_channel, kernel, stride, padding, groups, dilation]
-    def __init__(self, layer_name, c_in, c_out, k=1, s=1, p=None, g=1, d=1, activation=True):
+    def __init__(self, c_in, c_out, k=1, s=1, p=None, g=1, d=1, activation=True):
         super().__init__()
-        self.index = layer_name
         self.conv = nn.Conv2d(c_in, c_out, k, s, p, groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c_out)
         self.activation = nn.SiLU() if activation is True else nn.Identity()
@@ -15,9 +14,6 @@ class ConvLayer(nn.Module):
     def forward(self, x):
         # apply CBS block input and return the output
         return self.activation(self.bn(self.conv(x)))
-    
-    def get_index(self):
-        print(self.index)
 
 class Bottleneck(nn.Module):
     # class Bottleneck implements the block to reduce the number of channels in feature map using args:
@@ -25,8 +21,8 @@ class Bottleneck(nn.Module):
     def __init__(self, c_in, c_out, g=1, e=0.5):
         super().__init__()
         c_hidden = int(e * c_out)
-        self.conv1 = ConvLayer("bottleneck_1", c_in, c_hidden, 1, 1)
-        self.conv2 = ConvLayer("bottleneck_2", c_hidden, c_out, 3 ,1, g=g)
+        self.conv1 = ConvLayer(c_in, c_hidden, 1, 1)
+        self.conv2 = ConvLayer(c_hidden, c_out, 3 ,1, g=g)
 
     def forward(self, x):
         return self.conv2(self.conv1(x))
@@ -38,9 +34,9 @@ class C3Layer(nn.Module):
     def __init__(self, c_in, c_out, n=1, g=1, e=0.5):
         super().__init__()
         c_hidden = int(e * c_in)
-        self.conv1 = ConvLayer("C3_1", c_in, c_hidden, 1, 1)
-        self.conv2 = ConvLayer("C3_2", c_in, c_hidden, 1, 1)
-        self.conv3 = ConvLayer("c3_3", 2* c_hidden, c_out, 1, 1)
+        self.conv1 = ConvLayer(c_in, c_hidden, 1, 1)
+        self.conv2 = ConvLayer(c_in, c_hidden, 1, 1)
+        self.conv3 = ConvLayer(2* c_hidden, c_out, 1, 1)
         self.bottleneck = nn.Sequential(*(Bottleneck(c_hidden, c_hidden, 1, e=1) for _ in range(n)))
 
     def forward(self, x):
@@ -52,8 +48,8 @@ class SPPF(nn.Module):
     def __init__(self, c_in, c_out, e=2, k=5):
         super().__init__()
         c_hidden = c_in // e    # hidden channel size
-        self.conv1 = ConvLayer("SPPF_1", c_in, c_hidden, 1, 1)
-        self.conv2 = ConvLayer("SPPF_2", c_hidden *4 , c_out, 1, 1)
+        self.conv1 = ConvLayer(c_in, c_hidden, 1, 1)
+        self.conv2 = ConvLayer(c_hidden *4 , c_out, 1, 1)
         self.maxPool = nn.MaxPool2d(kernel_size=k, stride=1, padding=k//e)
 
     def forward(self, x):
