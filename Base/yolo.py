69698fc5f04857720e4626c57104f7db561d0582
Amirshayan1 <sh.tatari18@gmail.com>
2023-10-03 18:10:28 +0200

    parse_config returns complete sequential container

--- Base/yolo.py
+++ Base/yolo.py
@@ -17,31 +17,56 @@ class YamlParser:
             self.model_dic = yaml.safe_load(f)
 
     def parse_config(self, ch):
-        dic = deepcopy(self.model_dic)
         # get different components from the model dictionary
+        dic = deepcopy(self.model_dic)
+        
         # get number of classes
         num_classes = dic["num_classes"]
-
         print("Number of classes: {}".format(num_classes))
 
         # get the anchor boxes
         anchors = dic['anchors']
         print("Anchor boxes: {}".format(anchors))
         
-        print("Parsing backbone data:")
-        for iter, (input, num_layers, type, args) in enumerate(dic["backbone"]):
+        print("Parsing backbone and head data:")
+        layers = []
+        for iter, (input_from, num_layers, layer_type, args) in enumerate(dic["backbone"]):
             # create args list
             for indx,value in enumerate(args):
                 args[indx] = eval(value) if isinstance(value, str) else value
 
-            # eval type of the layer
-            type = eval(type)
-            if type in {ConvLayer, C3Layer, SPPF}:
+            # edit args lits based on the layer type 
+            layer_type = eval(layer_type)
+            if layer_type in {ConvLayer, C3Layer, SPPF}:
                 # assign the size of input and output channels       
-                c_in, c_out = ch[input], args[0]
+                c_in, c_out = ch[input_from], args[0]
                 args = [c_in, c_out, *args[1:]]
+                
+                # C3 layers has more than 1 layer
+                if layer_type is C3Layer:
+                    args.insert(2, num_layers)
 
-        
+            # create a sequential module of layers    
+            module = nn.Sequential(*(layer_type(*args) for _ in range(num_layers))) if num_layers > 1 else layer_type(*args)
+
+            # edit layer type and assign it to the module                 
+            module.layer_type = str(layer_type)[8:-2].replace('__main__.', '')
+
+            # calculate model parameters
+            module.num_params = sum(x.numel() for x in module.parameters())
+
+            # index, input from
+            module.index, module.input_from = iter, input_from
+            
+            # added module to the layer list
+            layers.append(module)
+
+            print("layer{}: type:{} with num_param:{} added to Sequential.".format(module.index, module.layer_type, module.num_params))
+
+        return nn.Sequential(*layers)
+
+                    
+                
             
 
 
