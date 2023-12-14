183fa6a7110e855f03e0095bf21cb9be19c08cf3
Amirshayan1 <sh.tatari18@gmail.com>
2023-12-14 07:58:41 +0100

    added a check for class names

--- data/data_wrapper.py
+++ data/data_wrapper.py
@@ -8,9 +8,13 @@ class DataWrapper():
 
     def __init__(self, data_yaml):
         self.hyper = parse_yaml(data_yaml)
+        self.class_names = self.hyper['class_names']
 
-    def create_datasets():
+    def create_datasets(self, cls_num):
         """
         Creates train, valid and test sets
         """
-        pass
+        
+        # check the class number with len of class names
+        assert len(self.class_names) == cls_num, "create_datasets: ERROR: class names & number of classes are different!"
+
