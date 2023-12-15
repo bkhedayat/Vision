06830530088c5802034944bc6d90a35c0e8a472b
Amirshayan1 <sh.tatari18@gmail.com>
2023-12-15 07:57:45 +0100

    added train/valid/test members to DataWrapper

--- data/data_wrapper.py
+++ data/data_wrapper.py
@@ -1,8 +1,13 @@
 from pathlib import Path
-from ..Utils.utils import parse_yaml
+import sys
 
 # create absolute ROOT path object of model
 ROOT = Path(__file__).resolve().parents[1]
+
+# add ROOT to sys path
+sys.path.append(str(ROOT))
+
+from Utils.utils import parse_yaml
 
 class DataWrapper():
     """
@@ -13,6 +18,9 @@ class DataWrapper():
     def __init__(self, data_yaml):
         self.hyper = parse_yaml(data_yaml)
         self.class_names = self.hyper['class_names']
+        self.train_dir = ROOT/self.hyper["train_dir"]
+        self.valid_dir = ROOT/self.hyper["valid_dir"]
+        self.test_dir = ROOT/self.hyper["test_dir"]
 
     def create_datasets(self, cls_num):
         """
@@ -21,4 +29,3 @@ class DataWrapper():
         
         # check the class number with len of class names
         assert len(self.class_names) == cls_num, "create_datasets: ERROR: class names & number of classes are different!"
-
