14a0e3c24e91cea1f11d2d84f50f4973f0e118cd
Amirshayan1 <sh.tatari18@gmail.com>
2023-12-18 08:01:36 +0100

    modified class Data to change the relative paths to absoloute in data dictionary

--- data/data_wrapper.py
+++ data/data_wrapper.py
@@ -9,23 +9,34 @@ sys.path.append(str(ROOT))
 
 from Utils.utils import parse_yaml
 
-class DataWrapper():
+class Data:
     """
-    dataset wrapper
-    recieves data yaml file
+    dataset wrapper recieves data yaml file
     """
-
     def __init__(self, data_yaml):
         self.hyper = parse_yaml(data_yaml)
         self.class_names = self.hyper['class_names']
-        self.train_dir = ROOT/self.hyper["train_dir"]
-        self.valid_dir = ROOT/self.hyper["valid_dir"]
-        self.test_dir = ROOT/self.hyper["test_dir"]
 
     def create_datasets(self, cls_num):
         """
         Creates train, valid and test sets
         """
-        
+
         # check the class number with len of class names
         assert len(self.class_names) == cls_num, "create_datasets: ERROR: class names & number of classes are different!"
+
+        # resolve the paths for train, val and test and make them absolute
+        for x in "train_dir", "valid_dir", "test_dir":
+            if self.hyper.get(x):
+                if isinstance(self.hyper[x], str):
+                    # resolve the path and store the string
+                    self.hyper[x] = str((ROOT/self.hyper[x]).resolve())
+                    print(self.hyper[x])
+
+            else:
+                print("create_datasets: ERROR: {} doest not exist in yaml!".format(x))
+                # exit the program
+                sys.exit(1)
+
+        # return dataset dictionary   
+        return self.hyper
