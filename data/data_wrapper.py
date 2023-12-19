818cde2b5a0db10fc7029f24a922df48efcb972c
Amirshayan1 <sh.tatari18@gmail.com>
2023-12-19 08:05:40 +0100

    prepare method creates dataset dirs

--- data/data_wrapper.py
+++ data/data_wrapper.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import sys
+import os, shutil
 
 # create absolute ROOT path object of model
 ROOT = Path(__file__).resolve().parents[1]
@@ -16,13 +17,24 @@ class Data:
     def __init__(self, data_yaml):
         self.hyper = parse_yaml(data_yaml)
         self.class_names = self.hyper['class_names']
-        self.main_data_dir = str(ROOT/"data/dataset/main")
+        self.main_data_dir = str((ROOT/"data/dataset/main").resolve())
 
-    def prepare(self, train_split=80):
+
+    def prepare(self, dir):
         """
         split the images into train/validation/test datasets
-        : param train_split: percentage of train data from all images
+        : param dir: path to train data directory
         """
+
+        # check if the train/val/test dirs are available and delete them
+        if os.path.exists(dir):
+            shutil.rmtree(dir)
+            
+        # create new sub dirs for images and labels
+        os.mkdir(dir)
+        os.mkdir(dir + "\\img")
+        os.mkdir(dir + "\\label")
+
 
     def create_datasets(self, cls_num):
         """
@@ -39,11 +51,20 @@ class Data:
                     # resolve the path and store the string
                     self.hyper[x] = str((ROOT/self.hyper[x]).resolve())
                     print(self.hyper[x])
+                    # prepare dir
+                    self.prepare(self.hyper[x])
 
             else:
                 print("create_datasets: ERROR: {} doest not exist in yaml!".format(x))
                 # exit the program
                 sys.exit(1)
 
+        
+
         # return dataset dictionary   
         return self.hyper
+
+if __name__ == "__main__":
+    hyper = str(ROOT/"data/data_aug.yaml")
+    data = Data(hyper)
+    data.create_datasets(1)
