62a82a612d1b8f59404a4166a49d87a54d8dd289
Amirshayan1 <sh.tatari18@gmail.com>
2023-12-21 07:55:55 +0100

    added method split_data to split data to train/val/test

--- data/data_wrapper.py
+++ data/data_wrapper.py
@@ -14,13 +14,13 @@ class Data:
     """
     dataset wrapper recieves data yaml file
     """
-    def __init__(self, data_yaml):
+    def __init__(self, data_yaml, split_ratio=0.8):
         self.hyper = parse_yaml(data_yaml)
         self.class_names = self.hyper['class_names']
         self.main_data_dir = str((ROOT/"data/dataset/main/").resolve())
         self.images = []
         self.annotations = []
-        self.train_split = 0.8
+        self.split_ratio = split_ratio
 
     def prepare(self, dir):
         """
@@ -60,6 +60,42 @@ class Data:
         # shuffle the index list
         random.shuffle(idxs)
         return idxs  
+    
+    def split_data(self, index_list):
+        """
+        split data using the split ratio and copy img + annot to 
+        correspondig directories
+        : param index_list: list of shuffled indexes of all data
+        """
+        # calculcate number of index for tarin data
+        num_train_data = int(len(index_list)*self.split_ratio)
+
+        # calculate number of index for valid data
+        num_valid_data = int(len(index_list)*(1-self.split_ratio)/2)
+
+        # split the images and annotation based on the train split
+        for num, idx in enumerate(index_list):
+            # create the absolute path for image and annotation files
+            img_path = self.main_data_dir + "/" + self.images[idx]
+            annot_path = self.main_data_dir + "/" + self.annotations[idx]
+            
+            # copy the files
+            if num < num_train_data:
+                # train dir"
+                shutil.copy(img_path, str(ROOT/"data/dataset/train/img"))
+                shutil.copy(annot_path, str(ROOT/"data/dataset/train/label"))
+                print("create_datasets: LOG: train data index {} added!".format(idx))
+            elif num >= num_train_data and num <= (num_train_data + num_valid_data):
+                # valid dir"
+                shutil.copy(img_path, str(ROOT/"data/dataset/valid/img"))
+                shutil.copy(annot_path, str(ROOT/"data/dataset/valid/label"))
+                print("create_datasets: LOG: valid data index {} added!".format(idx))
+            else:
+                # test dir
+                # train dir"
+                shutil.copy(img_path, str(ROOT/"data/dataset/test/img"))
+                shutil.copy(annot_path, str(ROOT/"data/dataset/test/label"))
+                print("create_datasets: LOG: test data index {} added!".format(idx))
 
     def create_datasets(self, cls_num):
         """
@@ -88,19 +124,13 @@ class Data:
         # get list of dataset indexs
         idxs = self.read_data()
         
-        # split the images and annotation based on the train split
-        for idx in range(int(len(idxs)*self.train_split)):
-            # copy the images and annotations in train dir"
-            img_path = self.main_data_dir + "/" + self.images[idxs[idx]]
-            annot_path = self.main_data_dir + "/" + self.annotations[idxs[idx]]
-            shutil.copy(img_path, str(ROOT/"data/dataset/train/img"))
-            shutil.copy(annot_path, str(ROOT/"data/dataset/train/label"))
-            print("create_datasets: LOG: img  label - {} copied!".format(idxs[idx]))              
+        # copy the train / valid / test data in corresponding dir
+        self.split_data(idxs)          
 
         # return dataset dictionary   
         return self.hyper
 
 if __name__ == "__main__":
     hyper = str(ROOT/"data/data_aug.yaml")
-    data = Data(hyper)
+    data = Data(hyper, 0.8)
     data.create_datasets(1)
