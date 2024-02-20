20bd6b9cf4258d1a3efb079e4580977933174284
Amirshayan1 <sh.tatari18@gmail.com>
2024-02-20 15:07:30 +0100

    code refactored

--- Utils/utils.py
+++ Utils/utils.py
@@ -1,39 +1,39 @@
+import sys
 import os
 import yaml
+from logger import *
 from copy import deepcopy
 
-def check_file(file):
-    """
-    checks if the file exists
-    """ 
-    # convert file to str format
-    file = str(file)
-    if os.path.isfile(file):
-        return file
-    else :
-        return ""
+def check_file(file) -> None:
+    """ Checks if the file exists. """ 
+    try:
+        # convert file to str format
+        file = str(file)
+        if os.path.isfile(file) == False:
+            raise Exception(f"check_file: {file} does not exist.")
+        LOGGER.info(f"check_file: {file} exists.")
+    except Exception as exp:
+        LOGGER.error(f"check_file: {type(exp)}, {exp}")
+        sys.exit(1)
     
 def parse_yaml(path):
-    """
-    parse the yaml file using path to the file
-    """
-    with open(path) as f:
-        # load the yaml file
-        loaded = yaml.safe_load(f)
-    
-    # make a deep copy of the parsed yaml file
-    loaded = deepcopy(loaded)
-    return loaded
+    """ Parse the yaml file using path to the file. """
+    try:
+        with open(path) as f:
+            # load the yaml file
+            loaded = yaml.safe_load(f)
+            LOGGER.info(f"parse_yaml: yaml file at {path} loaded.")
+            # make a deep copy of the parsed yaml file
+            return deepcopy(loaded)
+    except Exception as exp:
+        LOGGER.error(f"parse_yaml: {type(exp)}, {exp}")
+        sys.exit(1)
 
-def Check_input_size(img_input_size):
-    """
-    Checks whether the input size is divisable to 32
-
-    Args:
-        img_input_size(int): the input size of the image
-
-    Returns:
-        (bool)
-    """
-    reminder = int(img_input_size) % 32 
-    return True if reminder == 0 else False
+def Check_input_size(img_input_size) -> bool:
+    """ Checks whether the input size is divisable to 32. """
+    try:
+        reminder = int(img_input_size) % 32 
+        return True if reminder == 0 else False
+    except Exception as exp:
+        LOGGER.error(f"Check_input_size: {type(exp)}, {exp}")
+        sys.exit(1)
