6315c0082be4fc0ae1b77d87e88bb41c243841a3
Amirshayan1 <sh.tatari18@gmail.com>
2024-03-03 17:57:11 +0100

    code refactored

--- Base/neck.py
+++ Base/neck.py
@@ -1,11 +1,17 @@
 import torch
 import torch.nn as nn
 
+from Utils.utils import LOGGER
+
 class Concat(nn.Module):
-# class concat concatinates list of tensors along the given dimension
+    """ Concatinates list of tensors along the given dimension. """
     def __init__(self, dimention=1):
         super().__init__()
         self.dimension = dimention
 
     def forward(self, x):
-        return torch.cat(x, self.dimension)
+        try:
+            return torch.cat(x, self.dimension)
+        except Exception as exp:
+            LOGGER.error(f"Concat-forward: error: {type(exp)}: {exp}")
+            raise Exception("Concat-forward: failed!")
